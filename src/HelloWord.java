dfb7d2713ecfe9cc8bebdc156f89c8062b8f6f9a
cuidhui <18232916032@163.com>
2021-07-22 17:46:00 +0800

    commit文件到本地

--- src/HelloWord.java
+++ src/HelloWord.java
@@ -2,6 +2,7 @@ public class HelloWord {
     public static void main(String[] args) {
         System.out.println("张三");
         System.out.println("“王五");
+        System.out.println("3");
     }
 
 }
--- src/Test1.java
+++ src/Test1.java
@@ -2,6 +2,7 @@ public class Test1 {
 
     public static void main(String[] args) {
         System.out.println("李四");
+        System.out.println("4");
     }
 
 }
